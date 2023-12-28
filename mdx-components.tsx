4a636b776f4c31d116507e5ac03d84f007679b68
Burak Aslan <burakaslan@Buraks-MacBook-Pro.local>
2023-12-28 16:47:59 -0500

    Cont

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -8,9 +8,7 @@ import Image, { ImageProps } from "next/image";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h1: ({ children }) => (
-      <h1 style={{ fontSize: "100px" }}>asdasd{children}</h1>
-    ),
+    h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
     ...components,
   };
 }
