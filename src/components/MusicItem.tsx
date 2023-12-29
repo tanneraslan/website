fdb26d627dbbc892354d562a256eaf9085fddd79
tanneraslan <liontkaslan@gmail.com>
2023-12-29 13:17:37 -0500

    margin change

--- src/components/MusicItem.tsx
+++ src/components/MusicItem.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const MusicItem = (props: any) => {
   return (
-    <Link className="!no-underline" href={`/music/${props.id}`}>
+    <Link className="mb-8 block !no-underline" href={`/music/${props.id}`}>
       <div className="text-2xl">{props.title}</div>
       <div>{props.id}</div>
     </Link>
